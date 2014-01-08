72c7a1065d2ef971e28d6f7b259b90aeefb2b93a
banterle <frabante@gmail.com>
2014-01-08 11:52:14 +0100

    Ashikmin operator refactoring...

--- source_code/Tmo/AshikhminTMO.m
+++ source_code/Tmo/AshikhminTMO.m
@@ -32,11 +32,11 @@ function imgOut=AshikhminTMO(img, LdMax, pLocal)
 %is it a three color channels image?
 check13Color(img);
 
-if(~exist('pLocal'))
+if(~exist('pLocal','var'))
     pLocal=1;
 end
 
-if(~exist('LdMax'))
+if(~exist('LdMax','var'))
     LdMax=100;
 end
 
@@ -45,35 +45,7 @@ L=lum(img);
 
 %Local calculation?
 if(pLocal)
-    %precompute 10 filtered images
-    sMax=10;    %sMax should be one degree of visual angle, the value is set as in the original paper
-    [r,c]=size(L);
-    Lfiltered=zeros(r,c,sMax); %filtered images
-    LC=zeros(r,c,sMax);
-    for i=1:sMax
-       Lfiltered(:,:,i)=GaussianFilterWindow(L,i+1); 
-       %normalized difference of Gaussian levels
-       LC(:,:,i)=RemoveSpecials(abs(Lfiltered(:,:,i)-GaussianFilterWindow(L,(i+1)*2))./Lfiltered(:,:,i)); 
-    end  
-    
-    %threshold is a constant for solving the band-limited local contrast LC at a given
-    %image location. This is kept as in the original paper
-    threshold=0.5;
-    
-    %adaptation image
-    L_adapt=-ones(size(L));
-    for i=1:sMax
-        ind=find(LC(:,:,i)<threshold);
-        L_adapt(ind)=Lfiltered(r*c*(i-1)+ind);
-    end
-    
-    %set the maximum level
-    ind=find(L_adapt<0);
-    L_adapt(ind)=Lfiltered(r*c*(sMax-1)+ind);
-    
-    %Remove the detail layer
-    Ldetail=RemoveSpecials(L./L_adapt);
-    L=L_adapt;
+    [L,Ldetail] = AshikhminFiltering(L);
 end
 
 %Roboust maximum and minimum
@@ -84,7 +56,7 @@ minL=MaxQuart(L,0.0005);
 maxL_TVI=TVI_Ashikhmin(maxL);
 minL_TVI=TVI_Ashikhmin(minL);
 
-Ld=LdMax*(TVI_Ashikhmin(L)-minL_TVI)/(maxL_TVI-minL_TVI);
+Ld=(LdMax/100)*(TVI_Ashikhmin(L)-minL_TVI)/(maxL_TVI-minL_TVI);
 
 %Local Recombination
 if(pLocal)
