7ea21ed50b5ec45c00871e7d66842ef838506c02
banterle <frabante@gmail.com>
2014-04-03 11:51:47 +0200

    refactoring

--- source_code/Generation/RemovingGlare.m
+++ source_code/Generation/RemovingGlare.m
@@ -30,77 +30,19 @@ function [imgOut, imgGlare, PSF] = RemovingGlare( img )
 %
 %
 
-[r,c,col] = size(img);
+%estimating the PSF
+[PSF, C, hot_pixels_pos] = EstimatePSF( img );
 
+[r, c, col] = size(img);
 Icr = imresize(img, [round(r*128/c), 128], 'bilinear');
 
-Igr = lum(Icr); 
+%computing the glare image
+imgGlare = ComputeGlareImage(Icr, PSF, hot_pixels_pos);
 
-thr = 1000.0 * min(Igr(Igr>0.0));
+%upsampling the glare image
+imgGlare = imresize(imgGlare, [size(img, 1), size(img, 2)], 'bilinear');
 
-%getting hot pixels' positions
-[y_h, x_h] = find(Igr>thr);
-
-%getting dark pixels' positions
-[y_d, x_d] = find(Igr<=thr);
-
-m = length(y_h);
-n = length(y_d);
-A = zeros(n, 4);
-
-tot = sum(Igr(Igr>thr));
-
-for i=1:length(y_d)
-   A(i, 1) = tot;
-   
-   for j=1:m
-       r2 = (x_h(j) - x_d(i)).^2 + (y_h(j) - y_d(i)).^2;
-       r  = sqrt(r2);
-       
-       if(r>=3) 
-           P_j = Igr(y_h(j), x_h(j));
-
-           A(i, 2) = A(i, 2) + P_j/r;
-           A(i, 3) = A(i, 3) + P_j/r2;
-           A(i, 4) = A(i, 4) + P_j/(r2*r);
-       end
-   end
-end
-
-b = Igr(Igr<=thr);
-
-C = A\b;
-
-[x, y] = meshgrid(1:33,1:33);
-x = x - 16;
-y = y - 16;
-r = max(sqrt(x.^2 + y.^2),3);
-PSF = C(1) + C(2)./r + C(3)./(r.^2) + C(4)./(r.^3);
-
-col = zeros(m, 3);
-for i=1:m
-    col(i, :) = Icr(y_h(i), x_h(i), :); 
-end
-
-pos = [x_h'; y_h'];
-
-[imgOut, counter_map] = imSplat(size(Icr, 1), size(Icr, 2),PSF, pos, col);
-
-%compensation
-while(1)
-    ind = find(imgOut>Icr);
-       
-    if(isempty(ind))
-        break
-    end
-    
-    scale = Icr(ind(1)) / imgOut(ind(1)) * 0.99;
-    
-    imgOut = imgOut * scale;
-end 
-
-imgGlare = imresize(imgOut, [size(img, 1), size(img, 2)], 'bilinear');
-
+%removing glare
 imgOut = (img - imgGlare);
 
 end
