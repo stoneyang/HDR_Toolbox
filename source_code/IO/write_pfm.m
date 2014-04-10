0db79466e1110dfab65903bb9f1b121c187b7f10
banterle <frabante@gmail.com>
2014-04-10 14:12:03 +0200

    cleaning

--- source_code/IO/write_pfm.m
+++ source_code/IO/write_pfm.m
@@ -26,18 +26,19 @@ function ret = write_pfm(img,filename)
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 
-ret=0;
+ret = 0;
+
 %open the file
 fid = fopen(filename,'w');
 
-[n,m,c]=size(img);
+[n,m,c] = size(img);
 
 fprintf(fid,'PF%c%d %d%c-1.000000%c',10,m,n,10,10);
 
 tot = n*m;
-data=zeros(tot*3,1);
+data = zeros(tot*3,1);
 
-img=imrotate(img,-90,'nearest');
+img = imrotate(img,-90,'nearest');
 
 for i=1:c
     indx = i:3:(tot*3);
@@ -52,5 +53,7 @@ end
 fwrite(fid,data,'float');
 
 fclose(fid);
+
+ret = 1;
 
 end
