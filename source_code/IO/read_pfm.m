6a682fe70232a5d4cf9c2daf5a57e19d5dcd35c5
banterle <frabante@gmail.com>
2014-11-18 12:12:07 +0100

    added support for gray scale images in the PFM file format.

--- source_code/IO/read_pfm.m
+++ source_code/IO/read_pfm.m
@@ -1,15 +1,17 @@
-function img = read_pfm(filename)
+function img = read_pfm(filename, bPhotoshopCompatibility)
 %
 %
-%       img = read_pfm(filename)
+%       img = read_pfm(filename, bPhotoshopCompatibility)
 %
 %        Input:
 %           -filename: the name of the file to open
+%           -bPhotoshopCompatibility: a flag for enabling compability with
+%           Adobe Photoshop. If it is set to 1 the compability is on.
 %
 %        Output:
 %           -img: the opened image
 %
-%     Copyright (C) 2011  Francesco Banterle
+%     Copyright (C) 2011-14  Francesco Banterle
 % 
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
@@ -25,14 +27,42 @@ function img = read_pfm(filename)
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 
+if(~exist('bPhotoshopCompatibility', 'var'))
+    bPhotoshopCompatibility = 0;
+end
+
 %by default PFM files are open in little-endian format
 fid = fopen(filename, 'r', 'l');
 
 %reading the header
-fscanf(fid,'%c',3);
+str = fscanf(fid, '%c', 3);
+
+channels = 3;
+iValid = 0;
+
+if(str(1) == 'P')
+    iValid = 1;
+end
+
+if(str(2) == 'f')
+    channels = 1;
+    iValid = iValid + 1; 
+end
+
+if(str(2) == 'F')
+    iValid = iValid + 1; 
+end
+
+if(iValid ~= 2)
+    fclose(fid);
+    error('This is not a valid PFM file.');  
+end
+
+%image size
 m = fscanf(fid,'%d',1);
 fscanf(fid,'%c',1);
 n = fscanf(fid,'%d',1);
+
 fscanf(fid,'%c',1);
 endian_selector = fscanf(fid,'%f',1);
 fscanf(fid,'%c',1);
@@ -52,17 +82,23 @@ if(endian_selector > 0.0)
     fscanf(fid,'%c',1);  
 end
 
-img = zeros([m, n, 3]);
+img = zeros([m, n, channels]);
 
-total = n * m * 3;
+total = n * m * channels;
 tmpImg = fread(fid, total, 'float');
 
-for i=1:3
-    tmpC = i:3:total;
+for i=1:channels
+    tmpC = i:channels:total;
     img(:,:,i) = reshape(tmpImg(tmpC), m, n);    
 end
 
-img = imrotate(img,90,'nearest');
+img = imrotate(img, 90, 'nearest');
+
+if(bPhotoshopCompatibility)
+    for i=1:channels
+        img(:,:,i) = flipud(img(:,:,i));
+    end
+end
 
 fclose(fid);
 
--- source_code/IO/write_pfm.m
+++ source_code/IO/write_pfm.m
@@ -1,7 +1,6 @@
-function ret = write_pfm(img, filename, endian_mode)
+function ret = write_pfm(img, filename, endian_mode, bPhotoshopCompatibility)
 %
-%       ret = write_pfm(img, filename, endian_mode)
-%
+%       ret = write_pfm(img, filename, endian_mode, bPhotoshopCompatibility)
 %
 %        Input:
 %           -img: the image to write on the hard disk
@@ -9,6 +8,8 @@ function ret = write_pfm(img, filename, endian_mode)
 %           -endian_mode: sets the endian mode for writing float values:
 %                         - 'l': little-endian (default)
 %                         - 'b': big-endian
+%           -bPhotoshopCompatibility: a flag for enabling compability with
+%           Adobe Photoshop. If it is set to 1 the compability is on.
 %
 %        Output:
 %           -ret: a boolean value, it is set to 1 if the write succeed, 0 otherwise
@@ -33,33 +34,50 @@ if(~exist('endian_mode', 'var'))
     endian_mode = 'l';
 end
 
-%open the file
-fid = fopen(filename,'w', endian_mode);
+if(~exist('bPhotoshopCompatibility', 'var'))
+    bPhotoshopCompatibility = 0;
+end
 
-[n,m,c] = size(img);
+%open the file
+fid = fopen(filename, 'w', endian_mode);
+
+[n, m, c] = size(img);
+
+if(bPhotoshopCompatibility)
+    for i=1:c
+        img(:,:,i) = flipud(img(:,:,i));
+    end
+end
 
 switch endian_mode
     case 'l'
-        fprintf(fid,'PF%c%d %d%c-1.000000%c', 10, m, n, 10, 10);
+        if(c == 1)
+            fprintf(fid,'Pf%c%d %d%c-1.000000%c', 10, m, n, 10, 10);
+        else
+            fprintf(fid,'PF%c%d %d%c-1.000000%c', 10, m, n, 10, 10);
+        end
 
     case 'b'
-        fprintf(fid,'PF%c%d %d%c1.000000%c', 10, m, n, 10, 10);
+        if(c == 1)
+            fprintf(fid,'Pf%c%d %d%c1.000000%c', 10, m, n, 10, 10);
+        else
+            fprintf(fid,'PF%c%d %d%c1.000000%c', 10, m, n, 10, 10);
+        end
 end
 
 tot = n * m;
-tot3 = tot * 3;
+tot3 = tot * c;
 data = zeros(tot3, 1);
 
 img = imrotate(img, -90, 'nearest');
 
-
 for i=1:c
-    indx = i:3:tot3;
+    indx = i:c:tot3;
     data(indx) = reshape(img(:,:,i), tot, 1);
 end
 
-for i=c:3
-    indx = i:3:tot3;
+for i=1:c
+    indx = i:c:tot3;
     data(indx) = reshape(img(:,:,i), tot, 1);
 end
 
