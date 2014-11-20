696b9251011155ce4a10efd4220f6454da214c3e
banterle <frabante@gmail.com>
2014-11-20 10:46:11 +0100

    minor fixes

--- source_code/util/imSplat.m
+++ source_code/util/imSplat.m
@@ -1,6 +1,6 @@
-function [imgOut,counter_map] = imSplat(r,c,imSprite,splat_pos,splat_power)
+function [imgOut, counter_map] = imSplat(r, c, imSprite, splat_pos, splat_power)
 %
-%		 [imgOut,counter_map] = imSplat(r,c,imSprite,splat_pos,splat_power)
+%		 [imgOut, counter_map] = imSplat(r, c, imSprite, splat_pos, splat_power)
 %
 %
 %		 Input:
@@ -8,12 +8,13 @@ function [imgOut,counter_map] = imSplat(r,c,imSprite,splat_pos,splat_power)
 %           -c: the width of the output image
 %			-imSprite: an input image to be splat
 %           -splat_pos: splatting coordinates
+%           -splat_power: the power of each splat (optional)
 %
 %		 Output:
 %			-imgOut: the final image after splatting
 %           -counter_map: a map for counting splats per pixel
 %
-%     Copyright (C) 2012  Francesco Banterle
+%     Copyright (C) 2012-14  Francesco Banterle
 % 
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
@@ -29,57 +30,58 @@ function [imgOut,counter_map] = imSplat(r,c,imSprite,splat_pos,splat_power)
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 
-col = size(splat_power,2);
-[rS,cS] = size(imSprite);
+col = size(splat_power, 2);
 
-if(~exist('splat_power','var'))
-    splat_power = ones(size(splat_pos,2),col);
+if(~exist('splat_power', 'var'))
+    splat_power = ones(size(splat_pos, 2), col);
 end
 
 imgOut = zeros(r,c,col);
 counter_map = zeros(r,c);
 
-[rS,cS,colS] = size(imSprite);
-imSprite_counting = ones(rS,cS);
+[rS, cS, colS] = size(imSprite);
+imSprite_counting = ones(rS, cS);
 
-n = size(splat_pos,2);
+n = size(splat_pos, 2);
 
-if(mod(rS,2)==0)
-    rh = round(rS/2);
+if(mod(rS, 2) == 0)
+    rh = round(rS / 2);
     dy = -1;
 else
-    rh = floor(rS/2);    
+    rh = floor(rS / 2);    
     dy = 0;
 end
 
-if(mod(cS,2)==0)
-    ch = round(cS/2);
+if(mod(cS, 2)==0)
+    ch = round(cS / 2);
     dx = -1;
 else
-    ch = floor(cS/2);        
+    ch = floor(cS / 2);        
     dx = 0;
 end
 
 for i=1:n
-    x = splat_pos(1,i);
-    y = splat_pos(2,i);
+    x = splat_pos(1, i);
+    y = splat_pos(2, i);
     
-    minX = max([x-ch,1]);
-    maxX = min([x+ch+dx,c]);
+    minX = max([x - ch, 1]);
+    maxX = min([x + ch + dx, c]);
 
-    minY = max([y-rh,1]);
-    maxY = min([y+rh+dy,r]);
+    minY = max([y - rh, 1]);
+    maxY = min([y + rh + dy, r]);
 
-    diff_x = abs((x-ch)-minX);
-    diff_y = abs((y-rh)-minY);
-    y_coord = (minY:maxY)-minY+1+diff_y;
-    x_coord = (minX:maxX)-minX+1+diff_x;
+    diff_x = abs((x - ch) - minX);
+    diff_y = abs((y - rh) - minY);
+    y_coord = (minY:maxY) - minY + 1 + diff_y;
+    x_coord = (minX:maxX) - minX + 1 + diff_x;
     
     for j=1:col
-        imgOut(minY:maxY,minX:maxX,j) = imgOut(minY:maxY,minX:maxX,j) + splat_power(i,j).*imSprite(y_coord,x_coord);
+        imgOut(minY:maxY,minX:maxX,j) = imgOut(minY:maxY, minX:maxX, j) + ...
+                                        splat_power(i,j) .* imSprite(y_coord, x_coord);
     end
     
-    counter_map(minY:maxY,minX:maxX,:) = counter_map(minY:maxY,minX:maxX,:) + imSprite_counting(y_coord,x_coord,:);
+    counter_map(minY:maxY, minX:maxX,:) = counter_map(minY:maxY, minX:maxX,:) + ...
+                                          imSprite_counting(y_coord, x_coord,:);
 end
 
 counter_map(counter_map<1.0) = 1.0;
