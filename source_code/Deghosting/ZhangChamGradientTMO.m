8a7dbbfac2a71d37e256f3e2cc9b0eb36beb5fd5
banterle <frabante@gmail.com>
2015-04-19 16:49:52 +0200

    Update ZhangChamGradientTMO.m

--- source_code/Deghosting/ZhangChamGradientTMO.m
+++ source_code/Deghosting/ZhangChamGradientTMO.m
@@ -1,17 +1,16 @@
-function imgOut = ZhangChamGradientTMO(img, directory, format, imageStack, bStatic)
+function imgOut = ZhangChamGradientMerge(imageStack, directory, format, bStatic)
 %
 %
-%        imgOut = ZhangChamGradientTMO(img, directory, format, imageStack)
+%        imgOut = ZhangChamGradientMerge(imageStack, directory, format, bStatic)
 %
 %
 %        Input:
-%           -img: input HDR image
+%           -imageStack: a stack of LDR images
 %           -directory: the directory where to fetch the exposure imageStack in
-%           the case img=[]
+%           the case imageStack=[]
 %           -format: the format of LDR images ('bmp', 'jpg', etc) in case
-%                    img=[] and the tone mapped images is built from a sequence of
+%                    imageStack=[] and the tone mapped images is built from a sequence of
 %                    images in the current directory
-%           -imageStack:
 %           -bStatic: determing if the scene is static or not
 %
 %        Output:
@@ -20,7 +19,7 @@ function imgOut = ZhangChamGradientTMO(img, directory, format, imageStack, bStat
 %        Note: Gamma correction is not needed because it works on gamma
 %        corrected images.
 % 
-%     Copyright (C) 2013  Francesco Banterle
+%     Copyright (C) 2013-15  Francesco Banterle
 %
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
@@ -37,41 +36,36 @@ function imgOut = ZhangChamGradientTMO(img, directory, format, imageStack, bStat
 %
 
 %imageStack generation
-if(~exist('imageStack','var'))
+if(~exist('imageStack', 'var'))
     imageStack = [];
 end
 
-if(~exist('bStatic','var'))
+if(~exist('bStatic', 'var'))
     bStatic = 1;
 end
 
-if(~isempty(img))
-    %Convert the HDR image into a imageStack
-    [imageStack, imageStack_exposure] = GenerateExposureBracketing(img, 1);
-else
-    if(isempty(imageStack))
-        imageStack = ReadLDRStack(directory, format, 1);
-    end
+if(isempty(imageStack))
+    imageStack = ReadLDRStack(directory, format, 1);
 end
 
 %extracting gradients
-[r,c,col,n] = size(imageStack);
+[r, c, col, n] = size(imageStack);
 
-grad_mag = zeros(r,c,n);
-grad_ori = zeros(r,c,n);
+grad_mag = zeros(r, c, n);
+grad_ori = zeros(r, c, n);
 
-mag_total = zeros(r,c);
+mag_total = zeros(r, c);
 for i=1:n
     L = lum(imageStack(:,:,:,i));
     
     window_size = 5;
-    kernel_gauss = fspecial('gaussian',window_size,GKSigma(window_size));
+    kernel_gauss = fspecial('gaussian', window_size,GKSigma(window_size));
     [kgx,kgy] = gradient(kernel_gauss);
     
-    gx = imfilter(L,kgx,'replicate');
-    gy = imfilter(L,kgy,'replicate');
+    gx = imfilter(L, kgx, 'replicate');
+    gy = imfilter(L, kgy, 'replicate');
     
-    grad_mag(:,:,i) = sqrt(gx.^2+gy.^2);
+    grad_mag(:,:,i) = sqrt(gx.^2 + gy.^2);
     grad_ori(:,:,i) = atan2(gy, gx);
     
     mag_total = mag_total + grad_mag(:,:,i); 
@@ -81,9 +75,9 @@ epsilon = 1e-25; %as in the original paper;
 
 if(~bStatic)%Dynamic scene
     %Computing the visibility measure V_i; Equation 2 of the original paper
-    V = zeros(r,c,n);
+    V = zeros(r, c, n);
     for i=1:n
-        V(:,:,i) = grad_mag(:,:,i)./(mag_total+epsilon);
+        V(:,:,i) = grad_mag(:,:,i) ./ (mag_total + epsilon);
     end
     clear('grad_mag');
     clear('mag_total');
@@ -95,59 +89,59 @@ if(~bStatic)%Dynamic scene
         S_i = ZhangCham_Score(grad_ori, i);
         alpha_i = ZhangCham_Alpha(lum(imageStack(:,:,:,i)));
 
-        C(:,:,i) = S_i.*alpha_i;    
+        C(:,:,i) = S_i .* alpha_i;    
         C_total =  C_total + C(:,:,i);    
     end
 
     clear('grad_ori');
     
     for i=1:n
-        C(:,:,i) = C(:,:,i)./(C_total+epsilon);
+        C(:,:,i) = C(:,:,i) ./ (C_total + epsilon);
     end
 
     clear('C_total');
 
 
     %Final weights
-    W = zeros(r,c,n);
-    W_total = zeros(r,c);
+    W = zeros(r, c, n);
+    W_total = zeros(r, c);
 
     for i=1:n
-        W(:,:,i) = V(:,:,i).*C(:,:,i);
+        W(:,:,i) = V(:,:,i) .* C(:,:,i);
         W_total = W_total + W(:,:,i);
     end
 
     clear('V');
     clear('C');
 
-    imgOut = zeros(r,c,col);
-    total = zeros(r,c);
+    imgOut = zeros(r, c, col);
+    total = zeros(r, c);
     for i=1:n      
-        W_i = W(:,:,i)./W_total;
-        W_i = bilateralFilter(W_i,double(lum(imageStack(:,:,:,i))));%,0.0,1.0,5,5/255);
+        W_i = W(:,:,i) ./ W_total;
+        W_i = bilateralFilter(W_i, double(lum(imageStack(:,:,:,i))));%,0.0,1.0,5,5/255);
         for j=1:col
-            imgOut(:,:,j) = imgOut(:,:,j) + W_i.*imageStack(:,:,j,i);
+            imgOut(:,:,j) = imgOut(:,:,j) + W_i .* imageStack(:,:,j,i);
         end
         total = total + W_i;
     end
     
     for j=1:col
-        imgOut(:,:,j) = imgOut(:,:,j)./(total+epsilon);
+        imgOut(:,:,j) = imgOut(:,:,j) ./ (total + epsilon);
     end      
 else
-    imgOut = zeros(r,c,col);
-    total = zeros(r,c);
+    imgOut = zeros(r, c, col);
+    total = zeros(r, c);
     for i=1:n      
-        V_i = grad_mag(:,:,i)./(mag_total+epsilon);
-        V_i = bilateralFilter(V_i,double(lum(imageStack(:,:,:,i))));%,0.0,1.0,5,5/255);
+        V_i = grad_mag(:,:,i) ./ (mag_total + epsilon);
+        V_i = bilateralFilter(V_i, double(lum(imageStack(:,:,:,i))));%,0.0,1.0,5,5/255);
         for j=1:col
-            imgOut(:,:,j) = imgOut(:,:,j) + V_i.*imageStack(:,:,j,i);
+            imgOut(:,:,j) = imgOut(:,:,j) + V_i .* imageStack(:,:,j,i);
         end
         total = total + V_i;
     end
     
     for j=1:col
-        imgOut(:,:,j) = imgOut(:,:,j)./(total+epsilon);
+        imgOut(:,:,j) = imgOut(:,:,j) ./ (total + epsilon);
     end    
 end
 
