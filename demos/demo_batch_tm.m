74dc9f3978e5bfca5d249b7a829ce2c912f087ab
Yang Fan <stoneyang0915@gmail.com>
2015-05-04 16:02:56 +0800

    ENH: edited running configuration

--- demos/demo_batch_tm.m
+++ demos/demo_batch_tm.m
@@ -1,4 +1,5 @@
 clear all
+clc
 clf
 
 disp('====================Batch experiments of TMOs=====================');
@@ -6,12 +7,24 @@ disp('= 1) Load filenames of input files and output files;             =');
 disp('= 2) Calculate the dynamic range of input files;                 =');
 disp('= 3) Run the TMOs sequentially;                                  =');
 
-names = {'CS_Warwick', 'Oxford_Church'};
+names = {'Bottles_Small', 'CS_Warwick', 'groveC', 'groveD', 'memorial',...
+    'camp_daylight_10am_sanfran', 'camp_daylight_10am_sanfran_02',...
+    'camp_daylight_10am_sanfran_barn', 'camp_daylight_10am_sanfran_barn_006',...
+    'camp_daylight_10am_sanfran_barn_006_c', 'camp_daylight_10am_sanfran_barn_006_d',...
+    'camp_daylight_10am_sanfran_barn_007b', 'camp_daylight_10am_sanfran_barn_007c',...
+    'camp_daylight_10am_sanfran_barn_007d', 'camp_daylight_10am_sanfran_barn_007e',...
+    'camp_daylight_12pm_singapore', 'camp_daylight_12pm_singapore_4checkers_lamp',...
+    'camp_daylight_17', 'camp_night_full_glare_indoor',...
+    'camp_night_jeep_head_on', 'camp_night_jeep_head_on2',...
+    'nave', 'Oxford_Church', 'rosette', 'vinesunset'};, %'NotreDame1'
+
 TMOs = {'Ashikhmin', 'Banterle', 'Chiu', 'Drago', 'Durand', 'exponential',...
     'Fairchild', 'Fattal', 'Ferwerda', 'KimKautzConsistent', 'Krawczyk',...
     'Lischinski', 'logarithmic', 'normalize', 'Pattanaik', 'Raman',...
     'Reinhard', 'ReinhardBil', 'ReinhardDevlin', 'Schlick', 'TumblinRushmeier',...
     'VanHateren', 'WardGlobal', 'WardHistAdj', 'Yee'};
+
+records = cell(numel(TMOs), numel(names));
 
 inputPrefix = 'e:\yangfan\data\hdr\input\';
 inputSuffix = '.hdr';
@@ -24,6 +37,8 @@ outputHDRSuffix = '_hdr.tif';
 outputGammaSuffix = '_gamma.tif';
 outputTMSuffix = '.png';
 
+recordsFile = strcat(outputPrefix, 'records.csv');
+
 tic
 for i = 1 : numel(names)
     name = names{i};
@@ -33,16 +48,26 @@ for i = 1 : numel(names)
     outputHDR = strcat(outputPrefix, name, outputHDRSuffix);
     outputGamma = strcat(outputPrefix, name, outputGammaSuffix);
         
-    [dynRg, dynRgLog] = dynamic_range(inputFile, outputLum, outputLumMap);
+    [Lmax, Lmin, dynRg, dynRgLog] = dynamic_range(inputFile, outputLum, outputLumMap);
+    
+    records(1,i) = {name};
+    records(2,i) = {Lmax};
+    records(3,i) = {Lmin};
+    records(4,i) = {dynRg};
+    records(5,i) = {dynRg};
 
-    tic
     for j = 1 : numel(TMOs)
         TMO = TMOs{j};
         outputTMFile = strcat(outputPrefix, name, '_', TMO, outputTMSuffix);
         [imgTMO, execTime] = tone_mapping(inputFile, TMO, outputHDR, outputGamma, outputTMFile);
+        
+        records(j+5,i) = {execTime};
     end
 end
 avgTime = toc / numel(names);
+
+ds = cell2dataset(records);
+export(ds, 'file' ,recordsFile, 'delimiter', ',');
 
 disp('==============Summary of Batch experiments of TMOs================');
 disp(' ');
