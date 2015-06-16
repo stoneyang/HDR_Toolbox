64e08994d8289205a979810f53b6cd4fc0a852d1
Yang Fan <stoneyang0915@gmail.com>
2015-06-16 18:27:53 +0800

    ENH: inserted new lines for read

--- demos/demo_flare_removal.m
+++ demos/demo_flare_removal.m
@@ -1,11 +1,9 @@
 clear all;
 close all;
 clc;
-clf;
 
-hdrImg = hdrimread('e:\yangfan\data\hdr\input\trafficLight.hdr');
-[Lmax, Lmin, dynRg, dynRgLog] = dynamic_range(hdrImg, 'e:\yangfan\data\hdr\output\trafficLight_lum.tif', 'e:\yangfan\data\hdr\output\trafficLight_lum_map.tif');
+hdrImg = hdrimread('e:\yangfan\data\hdr\input\cathedral.hdr');
 [imgOut, imgGlare, PSF] = RemovingGlare(hdrImg);
 
-hdrimwrite(imgOut, 'e:\yangfan\data\hdr\input\trafficLight_flare_removed.hdr');
-hdrimwrite(imgGlare, 'e:\yangfan\data\hdr\input\trafficLight_glare.hdr');
+hdrimwrite(imgOut, 'e:\yangfan\data\hdr\input\cathedral_flare_removed.hdr');
+hdrimwrite(imgGlare, 'e:\yangfan\data\hdr\input\cathedral_glare.hdr');
