651125630535d5803a2afa980944b1e9bb377572
Yang Fan <stoneyang0915@gmail.com>
2015-06-17 17:25:14 +0800

    ENH: added new inputs & corrected output names

--- demos/GFTMO.m
+++ demos/GFTMO.m
@@ -1,18 +1,22 @@
 function [tmImg, I_est] = GFTMO(tmo, filename, medianRadius, GF_r, GF_eps, GF_detail, sigma, profile)
 
-inputDir = 'E:\yangfan\DATA\HDR\INPUT\';
-outputDir = 'E:\yangfan\DATA\HDR\OUTPUT\AD\';
-inputSuffix = '.hdr';
+% inputDir = 'E:\yangfan\DATA\HDR\INPUT\';
+% outputDir = 'E:\yangfan\DATA\HDR\OUTPUT\AD\';
+% inputSuffix = '.hdr';
+
+inputDir = 'E:\yangfan\DATA\WDR_raw_tif\INPUT\calibrated\';
+outputDir = 'E:\yangfan\DATA\WDR_raw_tif\OUTPUT\';
+inputSuffix = '.tif';
+
 outputSuffix = '.png';
+
 
 if (~exist('filename', 'file'))
     % % read HDR image as input -- single input temporarily
-    filename = 'trafficLight';
+    % filename = 'trafficLight';
     % filename = 'trafficLight_flare_removed'
     % filename = 'cathedral_sub';
     % filename = 'camp_daylight_10am_sanfran_barn_006_d';
-    % filename = 'E:\yangfan\DATA\HDR_IBL\HDR_110_Tunnel\HDR_110_Tunnel_Ref_ROI_1833_811_1610_972';
-    % filename = double(imread('E:\yangfan\DATA\WDR_raw_tif\INPUT\xueyuanroad_1920x1112_GR.tif'));
     % filename = 'office';
     % filename = 'belgium';
     % filename = 'bigFogMap';
@@ -20,8 +24,32 @@ if (~exist('filename', 'file'))
     % filename = 'BigI_big';
     % filename = 'AtriumMorning';
     % filename = 'AtriumNight';
+    
+    % filename = 'E:\yangfan\DATA\HDR_IBL\HDR_110_Tunnel\HDR_110_Tunnel_Ref_ROI_1833_811_1610_972';
+    
+    % filename = '1_1920x1112_GB_R95_G9_B97';
+    % filename = '2_1920x1112_GB_R95_G9_B97';
+    % filename = '3_1920x1112_GB_R95_G9_B97';
+    % filename = '4_1920x1112_GB_R085_G075_B1';
+    % filename = '10_1920x1112_GB_R09_G08_B09';
+    % filename = '11_1920x1112_GB_R075_G06_B08';
+    % filename = '12_1920x1112_GB_R1_G07_B1';
+    % filename = '13_1920x1112_GB_R08_G085_B085';
+    % filename = '17_1920x1112_GB_R1_G07_B1';
+    % filename = 'jingyihotel_1920x1112_GR_R95_G9_B97';
+    % filename = 'linear_raw_1936x1112_GR_2015-04-14-15-21-00-559_GR_R95_G9_B97';
+    % filename = 'linear_raw_1936x1112_GR_2015-04-14-15-28-35-184_R95_G9_B97';
+    % filename = 'linear_raw_1936x1112_GR_2015-04-14-16-00-12-715_R09_G06_B1';
+    % filename = 'linear_raw_1936x1112_GR_2015-04-14-18-43-13-375_R065_G05_B1';
+    % filename = 'shiningparking_1920x1112_GR_R95_G9_B97';
+    filename = 'xueyuanroad_1920x1112_GR_R95_G9_B97';
 end
-hdrImg = hdrimread(strcat(inputDir, filename, inputSuffix));
+
+if strcmp(inputSuffix, '.tif')
+    hdrImg = im2double(imread(strcat(inputDir, filename, inputSuffix)));
+else
+    hdrImg = hdrimread(strcat(inputDir, filename, inputSuffix));
+end
 I = hdrImg / 255;
 
 % smoothing the salt and pepper in each channels
@@ -92,6 +120,8 @@ switch tmo
     case 'Yang'
         [tmImg, ~] = YangTMO(enhancedImg);
         gammaImg = GammaTMO(tmImg, 2.4, 0, 0);
+    otherwise
+        disp('Unexpected tmo');
 end
 
 % noise removal using BM3D
@@ -114,8 +144,8 @@ figure; imshow(gammaImg);
 figure; imshow(I_est);
 
 % write the results
-gammaFilename = strcat(outputDir, filename, '_median', '_r', num2str(medianRadius), '_GF_r', num2str(GF_r), '_eps', num2str(GF_eps), '_detail', num2str(GF_detail), tmo, 'drago_b', num2str(dragoB), '_LdMax', num2str(dragoLdMax), '_Gamma', num2str(dragoGamma), outputSuffix);
-estFilename = strcat(outputDir, filename, '_median', '_r', num2str(medianRadius), '_GF_r', num2str(GF_r), '_eps', num2str(GF_eps), '_detail', num2str(GF_detail), tmo, 'drago_b', num2str(dragoB), '_LdMax', num2str(dragoLdMax), '_Gamma', num2str(dragoGamma), '_BM3D', '_sigma', num2str(sigma), outputSuffix);
+gammaFilename = strcat(outputDir, filename, '_median', '_r', num2str(medianRadius), '_GF_r', num2str(GF_r), '_eps', num2str(GF_eps), '_detail', num2str(GF_detail), '_', tmo, '_dragoB', num2str(dragoB), '_LdMax', num2str(dragoLdMax), '_Gamma', num2str(dragoGamma), outputSuffix);
+estFilename = strcat(outputDir, filename, '_median', '_r', num2str(medianRadius), '_GF_r', num2str(GF_r), '_eps', num2str(GF_eps), '_detail', num2str(GF_detail), '_', tmo, '_dragoB', num2str(dragoB), '_LdMax', num2str(dragoLdMax), '_Gamma', num2str(dragoGamma), '_BM3D', '_sigma', num2str(sigma), outputSuffix);
 imwrite(gammaImg, gammaFilename);
 imwrite(I_est, estFilename);
 
