53dda2018a495f1bb0c7a3eb4770918dc734a87b
Yang Fan <stoneyang0915@gmail.com>
2015-09-16 15:47:40 +0800

    DEL: removed unwanted scripts

--- demos/demo_flare_removal.m
+++
@@ -1,9 +0,0 @@
-clear all;
-close all;
-clc;
-
-hdrImg = hdrimread('e:\yangfan\data\hdr\input\cathedral.hdr');
-[imgOut, imgGlare, PSF] = RemovingGlare(hdrImg);
-
-hdrimwrite(imgOut, 'e:\yangfan\data\hdr\input\cathedral_flare_removed.hdr');
-hdrimwrite(imgGlare, 'e:\yangfan\data\hdr\input\cathedral_glare.hdr');
